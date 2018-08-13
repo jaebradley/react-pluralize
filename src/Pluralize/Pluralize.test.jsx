e621840918047a8a5cd26a72fb2487fca6c0f8a0
Jae Bradley <jae.b.bradley@gmail.com>
2018-08-12 18:23:26 -0700

    test(pluralize): add tests for component

--- src/Pluralize/Pluralize.test.jsx
+++ src/Pluralize/Pluralize.test.jsx
@@ -4,8 +4,33 @@ import { mount } from 'enzyme';
 import Pluralize from './index';
 
 describe('Pluralize', () => {
-  it('is a div', () => {
+  it('contains a div', () => {
     const example = mount(<Pluralize word="test" count={1} />);
     expect(example.find('div').length).toEqual(1);
+    expect(example.text()).toEqual('1 test');
+  });
+
+  describe('includes count', () => {
+    it('contains single value', () => {
+      const example = mount(<Pluralize word="test" count={1} />);
+      expect(example.text()).toEqual('1 test');
+    });
+
+    it('contains multiple values', () => {
+      const example = mount(<Pluralize word="test" count={5} />);
+      expect(example.text()).toEqual('5 tests');
+    });
+  });
+
+  describe('does not include count', () => {
+    it('contains single value', () => {
+      const example = mount(<Pluralize word="test" count={1} includeCounts={false} />);
+      expect(example.text()).toEqual('test');
+    });
+
+    it('contains multiple values', () => {
+      const example = mount(<Pluralize word="test" count={5} includeCounts={false} />);
+      expect(example.text()).toEqual('tests');
+    });
   });
 });
